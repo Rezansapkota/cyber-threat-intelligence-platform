34bcf36b3cc543d31aae1cff28c1858a07606307
rejan <rejansapkota9@gmail.com>
2026-05-22 23:14:55 +0800

    Harden Render startup defaults

--- cyber_project/wsgi.py
+++ cyber_project/wsgi.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cyber_project.settings')
 
 application = get_wsgi_application()
+
+if os.environ.get('RUN_STARTUP_TASKS', 'True').lower() == 'true':
+    call_command('migrate', interactive=False, verbosity=0)
+    call_command('ensure_demo_user', verbosity=0)
